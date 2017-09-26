55335a4b0096901a2d84184d8fdc357f3cd42e60
Alonso Avotz <alonso@avotz.com>
2017-09-25 18:15:00 -0600

    se agrego defaulstrinlen

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        Schema::defaultStringLength(191);
+        
         $countries = $this->getCountries();
 
         View::share('countries', $countries);
